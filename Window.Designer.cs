dba2e9350e1c6ffa338786a885c8a515df3e3fd7
DanSoW <67583913+DanSoW@users.noreply.github.com>
2021-10-16 18:42:30 +0800

    Добавление нового функционала.

--- Window.Designer.cs
+++ Window.Designer.cs
@@ -29,36 +29,29 @@ namespace DataBaseGenerator
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this._txtConnectionString = new System.Windows.Forms.TextBox();
 			this._txtQuery = new System.Windows.Forms.TextBox();
 			this._btnExecuteQuery = new System.Windows.Forms.Button();
 			this._dgvDatabase = new System.Windows.Forms.DataGridView();
 			this._tcControl = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
-			this.tabPage2 = new System.Windows.Forms.TabPage();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
 			this._btnFindFile = new System.Windows.Forms.Button();
-			this._txtNameServer = new System.Windows.Forms.TextBox();
-			this.label2 = new System.Windows.Forms.Label();
-			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this._txtFilePath = new System.Windows.Forms.TextBox();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
 			this.label3 = new System.Windows.Forms.Label();
+			this._cmbListDB = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this._txtNameServer = new System.Windows.Forms.TextBox();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
 			((System.ComponentModel.ISupportInitialize)(this._dgvDatabase)).BeginInit();
 			this._tcControl.SuspendLayout();
 			this.tabPage1.SuspendLayout();
 			this.tabPage2.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// _txtConnectionString
-			// 
-			this._txtConnectionString.Location = new System.Drawing.Point(12, 173);
-			this._txtConnectionString.Name = "_txtConnectionString";
-			this._txtConnectionString.Size = new System.Drawing.Size(824, 22);
-			this._txtConnectionString.TabIndex = 0;
-			// 
 			// _txtQuery
 			// 
-			this._txtQuery.Location = new System.Drawing.Point(12, 201);
+			this._txtQuery.Location = new System.Drawing.Point(9, 182);
 			this._txtQuery.Multiline = true;
 			this._txtQuery.Name = "_txtQuery";
 			this._txtQuery.Size = new System.Drawing.Size(824, 253);
@@ -66,7 +59,7 @@ namespace DataBaseGenerator
 			// 
 			// _btnExecuteQuery
 			// 
-			this._btnExecuteQuery.Location = new System.Drawing.Point(664, 475);
+			this._btnExecuteQuery.Location = new System.Drawing.Point(664, 441);
 			this._btnExecuteQuery.Name = "_btnExecuteQuery";
 			this._btnExecuteQuery.Size = new System.Drawing.Size(169, 38);
 			this._btnExecuteQuery.TabIndex = 2;
@@ -88,45 +81,35 @@ namespace DataBaseGenerator
 			// 
 			this._tcControl.Controls.Add(this.tabPage1);
 			this._tcControl.Controls.Add(this.tabPage2);
-			this._tcControl.Location = new System.Drawing.Point(15, 13);
+			this._tcControl.Location = new System.Drawing.Point(9, 13);
 			this._tcControl.Name = "_tcControl";
 			this._tcControl.SelectedIndex = 0;
-			this._tcControl.Size = new System.Drawing.Size(821, 151);
+			this._tcControl.Size = new System.Drawing.Size(830, 151);
 			this._tcControl.TabIndex = 4;
+			this._tcControl.SelectedIndexChanged += new System.EventHandler(this._tcControl_SelectedIndexChanged);
 			// 
 			// tabPage1
 			// 
 			this.tabPage1.Controls.Add(this._btnFindFile);
 			this.tabPage1.Controls.Add(this.label1);
-			this.tabPage1.Controls.Add(this.textBox1);
+			this.tabPage1.Controls.Add(this._txtFilePath);
 			this.tabPage1.Location = new System.Drawing.Point(4, 25);
 			this.tabPage1.Name = "tabPage1";
 			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage1.Size = new System.Drawing.Size(813, 122);
+			this.tabPage1.Size = new System.Drawing.Size(822, 122);
 			this.tabPage1.TabIndex = 0;
 			this.tabPage1.Text = "Локальная база";
 			this.tabPage1.UseVisualStyleBackColor = true;
 			// 
-			// tabPage2
+			// _btnFindFile
 			// 
-			this.tabPage2.Controls.Add(this.label3);
-			this.tabPage2.Controls.Add(this.comboBox1);
-			this.tabPage2.Controls.Add(this.label2);
-			this.tabPage2.Controls.Add(this._txtNameServer);
-			this.tabPage2.Location = new System.Drawing.Point(4, 25);
-			this.tabPage2.Name = "tabPage2";
-			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage2.Size = new System.Drawing.Size(813, 122);
-			this.tabPage2.TabIndex = 1;
-			this.tabPage2.Text = "Сервер";
-			this.tabPage2.UseVisualStyleBackColor = true;
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(19, 64);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(660, 22);
-			this.textBox1.TabIndex = 0;
+			this._btnFindFile.Location = new System.Drawing.Point(716, 63);
+			this._btnFindFile.Name = "_btnFindFile";
+			this._btnFindFile.Size = new System.Drawing.Size(75, 23);
+			this._btnFindFile.TabIndex = 2;
+			this._btnFindFile.Text = "...";
+			this._btnFindFile.UseVisualStyleBackColor = true;
+			this._btnFindFile.Click += new System.EventHandler(this._btnFindFile_Click);
 			// 
 			// label1
 			// 
@@ -137,38 +120,26 @@ namespace DataBaseGenerator
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Путь к файлу";
 			// 
-			// _btnFindFile
+			// _txtFilePath
 			// 
-			this._btnFindFile.Location = new System.Drawing.Point(716, 63);
-			this._btnFindFile.Name = "_btnFindFile";
-			this._btnFindFile.Size = new System.Drawing.Size(75, 23);
-			this._btnFindFile.TabIndex = 2;
-			this._btnFindFile.Text = "...";
-			this._btnFindFile.UseVisualStyleBackColor = true;
+			this._txtFilePath.Location = new System.Drawing.Point(19, 64);
+			this._txtFilePath.Name = "_txtFilePath";
+			this._txtFilePath.Size = new System.Drawing.Size(660, 22);
+			this._txtFilePath.TabIndex = 0;
 			// 
-			// _txtNameServer
+			// tabPage2
 			// 
-			this._txtNameServer.Location = new System.Drawing.Point(20, 66);
-			this._txtNameServer.Name = "_txtNameServer";
-			this._txtNameServer.Size = new System.Drawing.Size(336, 22);
-			this._txtNameServer.TabIndex = 0;
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(20, 43);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(93, 17);
-			this.label2.TabIndex = 1;
-			this.label2.Text = "Имя сервера";
-			// 
-			// comboBox1
-			// 
-			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Location = new System.Drawing.Point(393, 66);
-			this.comboBox1.Name = "comboBox1";
-			this.comboBox1.Size = new System.Drawing.Size(225, 24);
-			this.comboBox1.TabIndex = 2;
+			this.tabPage2.Controls.Add(this.label3);
+			this.tabPage2.Controls.Add(this._cmbListDB);
+			this.tabPage2.Controls.Add(this.label2);
+			this.tabPage2.Controls.Add(this._txtNameServer);
+			this.tabPage2.Location = new System.Drawing.Point(4, 25);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(822, 122);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Сервер";
+			this.tabPage2.UseVisualStyleBackColor = true;
 			// 
 			// label3
 			// 
@@ -179,6 +150,37 @@ namespace DataBaseGenerator
 			this.label3.TabIndex = 3;
 			this.label3.Text = "Название базы данных";
 			// 
+			// _cmbListDB
+			// 
+			this._cmbListDB.FormattingEnabled = true;
+			this._cmbListDB.Location = new System.Drawing.Point(393, 66);
+			this._cmbListDB.Name = "_cmbListDB";
+			this._cmbListDB.Size = new System.Drawing.Size(225, 24);
+			this._cmbListDB.TabIndex = 2;
+			this._cmbListDB.SelectedIndexChanged += new System.EventHandler(this._cmbListDB_SelectedIndexChanged);
+			this._cmbListDB.Click += new System.EventHandler(this.comboBox1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(20, 43);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(93, 17);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Имя сервера";
+			// 
+			// _txtNameServer
+			// 
+			this._txtNameServer.Location = new System.Drawing.Point(20, 66);
+			this._txtNameServer.Name = "_txtNameServer";
+			this._txtNameServer.Size = new System.Drawing.Size(336, 22);
+			this._txtNameServer.TabIndex = 0;
+			this._txtNameServer.TextChanged += new System.EventHandler(this._txtNameServer_TextChanged);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
 			// _Window
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -188,7 +190,6 @@ namespace DataBaseGenerator
 			this.Controls.Add(this._dgvDatabase);
 			this.Controls.Add(this._btnExecuteQuery);
 			this.Controls.Add(this._txtQuery);
-			this.Controls.Add(this._txtConnectionString);
 			this.Name = "_Window";
 			this.Text = "Database Generator";
 			((System.ComponentModel.ISupportInitialize)(this._dgvDatabase)).EndInit();
@@ -203,8 +204,6 @@ namespace DataBaseGenerator
 		}
 
 		#endregion
-
-		private System.Windows.Forms.TextBox _txtConnectionString;
 		private System.Windows.Forms.TextBox _txtQuery;
 		private System.Windows.Forms.Button _btnExecuteQuery;
 		private System.Windows.Forms.DataGridView _dgvDatabase;
@@ -212,12 +211,13 @@ namespace DataBaseGenerator
 		private System.Windows.Forms.TabPage tabPage1;
 		private System.Windows.Forms.Button _btnFindFile;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox _txtFilePath;
 		private System.Windows.Forms.TabPage tabPage2;
 		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ComboBox _cmbListDB;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.TextBox _txtNameServer;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
 	}
 }
 
--- Window.cs
+++ Window.cs
@@ -11,20 +11,42 @@ using System.Windows.Forms;
 
 namespace DataBaseGenerator
 {
+	/// <summary>
+	/// Основной класс главного окна
+	/// </summary>
 	public partial class _Window : Form
 	{
+		// Строка подключения к базе данных
+		private SqlConnectionStringBuilder _sqlConnectionString = new SqlConnectionStringBuilder();
+
+		// Название локального сервера
+		// public const string NAME_LOCAL_SERVER = @"(localdb)\MSSQLLocalDB";
+
 		public _Window()
 		{
+			// Инициализация компонентов на форме
 			InitializeComponent();
+
+			// Настройка фильтра
+			openFileDialog1.Filter = "Database files(*.mdf)|*.mdf";
+
+			// Установка стандартных данных для строки подключения
+			_sqlConnectionString.IntegratedSecurity = true;
+			_sqlConnectionString.ConnectTimeout = 2;
 		}
 
+		/// <summary>
+		/// Выполнение SQL-запроса
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void _btnExecuteQuery_Click(object sender, EventArgs e)
 		{
 			try
 			{
 				using (SqlConnection connection = new SqlConnection())
 				{
-					connection.ConnectionString = _txtConnectionString.Text;
+					connection.ConnectionString = _sqlConnectionString.ToString();
 					connection.Open();
 
 					SqlCommand command = new SqlCommand(_txtQuery.Text, connection);
@@ -33,14 +55,97 @@ namespace DataBaseGenerator
 						DataTable table = new DataTable();
 						table.Load(reader);
 						_dgvDatabase.DataSource = table;
+
+						reader.Close();
 					}
+
+					connection.Close();
 				}
 			}
-			catch
+			catch(Exception error)
 			{
-				MessageBox.Show("Не удалось подключиться к базе данных!",
+				MessageBox.Show("При выполнении операции возникла ошибка! \n" + error.Message,
 					"Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
+		}
+
+		/// <summary>
+		/// Открытие файла базы данных
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void _btnFindFile_Click(object sender, EventArgs e)
+		{
+			// Отображение OpenFileDialog для выбора файла 
+			if(openFileDialog1.ShowDialog() == DialogResult.Cancel)
+			{
+				_sqlConnectionString.AttachDBFilename = @"";
+				_sqlConnectionString.DataSource = @"";
+				return;
+			}
+
+			_txtFilePath.Text = openFileDialog1.FileName;
+			_sqlConnectionString.DataSource = @"(localdb)\MSSQLLocalDB";
+			_sqlConnectionString.AttachDBFilename = openFileDialog1.FileName;
+		}
+
+		/// <summary>
+		/// Получение списка баз данных
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void comboBox1_Click(object sender, EventArgs e)
+		{
+			_cmbListDB.Items.Clear();
+			try
+			{
+				using (SqlConnection connection = new SqlConnection())
+				{
+					connection.ConnectionString = _sqlConnectionString.ToString();
+					connection.Open();
+
+					using (SqlCommand cmd = new SqlCommand("SELECT name from sys.databases", connection))
+					{
+						using (IDataReader dr = cmd.ExecuteReader())
+						{
+							while (dr.Read())
+							{
+								_cmbListDB.Items.Add(dr[0].ToString());
+							}
+
+							dr.Close();
+						}
+					}
+
+					connection.Close();
+				}
+			}
+			catch (Exception error)
+			{
+				MessageBox.Show("При выполнении операции возникла ошибка! \n" + error.Message,
+					"Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		private void _tcControl_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			_sqlConnectionString.AttachDBFilename = @"";
+			_sqlConnectionString.DataSource = @"";
+			_sqlConnectionString.InitialCatalog = @"";
+
+			_txtNameServer.Text = "";
+			_cmbListDB.Items.Clear();
+			_txtFilePath.Text = "";
+		}
+
+		private void _txtNameServer_TextChanged(object sender, EventArgs e)
+		{
+			_sqlConnectionString.DataSource = @_txtNameServer.Text;
+		}
+
+		private void _cmbListDB_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			_sqlConnectionString.InitialCatalog = _cmbListDB.SelectedItem.ToString();
 		}
 	}
 }
